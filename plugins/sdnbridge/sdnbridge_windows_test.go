00c547cba06997ead4c66a801e95ae5c00c0b91a
kestratt <kestratt@microsoft.com>
2024-03-06 17:25:36 -0800

    set dualstack for workflow tests

--- plugins/sdnbridge/sdnbridge_windows_test.go
+++ plugins/sdnbridge/sdnbridge_windows_test.go
@@ -22,7 +22,7 @@ func CreateBridgeTestNetwork() *hcn.HostComputeNetwork {
 
 func TestBridgeCmdAdd(t *testing.T) {
 	// t.Skip("Bridge test is disabled for now.")
-	testDualStack = (os.Getenv("TestDualStack") == "1")
+	testDualStack = (os.Getenv("TestDualStackL2B") == "1")
 	imageToUse = os.Getenv("ImageToUse")
 	testNetwork := CreateBridgeTestNetwork()
 	pt := util.MakeTestStruct(t, testNetwork, "L2Bridge", true, true, "", testDualStack, imageToUse)
